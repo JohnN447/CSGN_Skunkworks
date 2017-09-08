e2e98fb41c11a7b28ce7184fa0891cfe6e639aee
Unknown <saint.github@outlook.at>
2017-09-08 15:47:09 +0200

    Removed useless stringtables + removed module from old whitelist system

--- CSGN_CorePack/addons/CSGN_CP_Core/cfgPatches.cpp
+++ CSGN_CorePack/addons/CSGN_CP_Core/cfgPatches.cpp
@@ -2,9 +2,7 @@ class CSGN_CP_Core
 {
     author = "[CSGN] DEV-Team";
     vehicles[] = {};
-    units[] = {
-        "CSGN_CP_wlInitM"
-    };
+    units[] = {};
     weapons[] = {};
     requiredVersion = 1.0;
     requiredAddons[] = {
--- CSGN_CorePack/addons/CSGN_CP_Core/cfgVehicles.cpp
+++ CSGN_CorePack/addons/CSGN_CP_Core/cfgVehicles.cpp
@@ -3,31 +3,3 @@ class Logic;
 class Module_F: Logic {
     class ModuleDescription;
 };
-
-class CSGN_CP_wlInitM: Module_F {
-    scope = 2;
-    displayName = $STR_CSGN_CP_wlInitM;
-    //icon = "\myTag_addonName\data\iconNuke_ca.paa";
-    category = "NO_CATEGORY";
-
-    function = "CSGN_fnc_wlInit";
-    functionPriority = 1;
-    isGlobal = 0;
-    isTriggerActivated = 0;
-    isDisposable = 0;
-
-    curatorInfoType = "";
-
-    class Arguments {
-        class CSGN_CP_wlInitM_OnOff {
-            displayName = $STR_CSGN_CP_wlInitM_OnOff_Name;
-            description = $STR_CSGN_CP_wlInitM_OnOff_Description;
-            typeName = "BOOL";
-            defaultValue = 0;
-        };
-    };
-
-    class ModuleDescription: ModuleDescription {
-        description = "Short module description";
-    };
-};
